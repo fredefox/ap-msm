2f5c1b6ac7be504e4de1741651b16378606b168f
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-09 14:35:29 +0200

    Define module

---
+++ MSM.hs
@@ -0,0 +1,20 @@
+{-
+ - ***
+ -
+ - This module implements all the functions of the curve-library.
+ -
+ - The implementation follows the specification located at:
+ -
+ -     [advanced programming course homepage](http://www.diku.dk/~kflarsen/ap-2014/notes/msm.html)
+ -
+ - Written by Frederik Hanghøj Iversen
+ - for the course Advanced Programming
+ - at The University of Copenhagen 2014
+ -
+ - me@fredefox.eu /^._
+ -  ,___,--~~~~--' /'~
+ -  `~--~\ )___,)/'
+ -      (/\\_  (/\\_
+ -
+ -}
+module MSM where
