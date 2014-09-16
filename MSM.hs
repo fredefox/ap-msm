48d171e5d37d7d8bcfe5d47f11c73f08bf095e60
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-16 19:17:56 +0200

    `runMSM` on the empty program should be undefined.
    How could it be anything else?

--- MSM.hs
+++ MSM.hs
@@ -333,6 +333,7 @@ interp = run
  -}
 -- This does not type-check and therefore it is outcommented.
 runMSM :: Prog -> Either Error Int
+runMSM [] = undefined
 runMSM p =
 	let (MSM f) = interp
 	    ethrETpl = f $ initial p
