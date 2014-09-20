458150b06c9f5962d25d25206c4b7cb7917a7dc4
Frederik Hanghøj Iversen <fhi.1990@gmail.com>
2014-09-20 15:14:36 +0200

    Add Test-module

---
+++ Test.hs
@@ -0,0 +1,22 @@
+{-
+ - ***
+ -
+ - This module tests the implementation of the Micro Stack Machine
+ -
+ - Written by Frederik Hanghøj Iversen
+ - for the course Advanced Programming
+ - at The University of Copenhagen 2014
+ -
+ - me@fredefox.eu /^._
+ -  ,___,--~~~~--' /'~
+ -  `~--~\ )___,)/'
+ -      (/\\_  (/\\_
+ -
+ -}
+module Test where
+
+import Test.HUnit
+import MSM
+import qualified Data.Map as Map
+
+
